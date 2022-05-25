748dac2e3c0b9ca3700750e2707d6ce1ffc05302
Oli Knight <olidknight@gmail.com>
2022-05-25 16:08:56 +0100

    Add chakra UI

--- client/.next/static/chunks/webpack.js
+++ client/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "d191caa471d0fb09"; }
+/******/ 		__webpack_require__.h = function() { return "0633112ac0f04ee5"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,7 +1,19 @@
+
+import { ChakraProvider } from '@chakra-ui/react'
+import { extendTheme } from '@chakra-ui/react'
 import type { AppProps } from 'next/app'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps } : AppProps) {
+
+const colors = {
+}
+
+const theme = extendTheme({ colors });
+  return (
+    <ChakraProvider theme={theme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  )
 }
 
 export default MyApp
