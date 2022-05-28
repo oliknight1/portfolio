32abb36293de1fcdf00b76b4157c9c23f6d75a63
Oli Knight <olidknight@gmail.com>
2022-05-28 15:40:53 +0100

    Refactor social icons

--- client/components/SocialsList.tsx
+++ client/components/SocialsList.tsx
@@ -5,23 +5,34 @@ import React, { FC } from 'react';
 import { GithubIcon, LinkedInIcon } from '../utils/icons';
 import { vertical_line } from '../styles/general';
 
+type SocialIcon = {
+	id: number,
+	name: string,
+	icon : React.ReactElement<any, string | React.JSXElementConstructor<any>>,
+	url: string
+}
+
 const SocialsList: FC = () => {
-	const icons = [ {
-		name: 'LinkedIn',
-		icon: <LinkedInIcon w={5} h={5} />,
-		url: 'https://www.linkedin.com/in/oliver-knight-13804b176/',
-	}, {
-		name: 'Github',
-		icon: <GithubIcon w={6} h={6} />,
-		url: 'https://github.com/oliknight1',
-	},
+	const icons : SocialIcon[] = [
+		{
+			id: 0,
+			name: 'LinkedIn',
+			icon: <LinkedInIcon w={5} h={5} />,
+			url: 'https://www.linkedin.com/in/oliver-knight-13804b176/',
+		},
+		{
+			id: 1,
+			name: 'Github',
+			icon: <GithubIcon w={6} h={6} />,
+			url: 'https://github.com/oliknight1',
+		},
 	];
 	return (
 		<Flex pos="fixed" bottom={6} left={20} flexDir="column">
 			<VStack _after={{ ...vertical_line, marginTop: '15px' }}>
 				{
 					icons.map( ( icon ) => (
-						<Link href={icon.url} isExternal>
+						<Link href={icon.url} isExternal key={icon.id}>
 							<IconButton variant="unstyled" _hover={{ color: 'brand.red', transform: 'translateY(-5px) scale(1.1)' }} aria-label={icon.name} icon={icon.icon} />
 						</Link>
 
