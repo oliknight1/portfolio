4c3044770344ac3a0e6293a2149a5389aa10bd7a
Oli Knight <olidknight@gmail.com>
2022-05-29 16:02:23 +0100

    Add subheading to header component

--- client/components/Header.tsx
+++ client/components/Header.tsx
@@ -9,7 +9,8 @@ const Header : FC = () => (
 	<Flex height="100vh" alignItems="center">
 		<div>
 			<Subheading mb={4}>Hi, my name is</Subheading>
-			<Heading as="h1" fontWeight="medium" fontSize="6rem" mb={6}>Oli Knight</Heading>
+			<Heading as="h1" fontWeight="medium" fontSize="6rem">Oli Knight</Heading>
+			<Heading as="h2" fontWeight="medium" fontSize="4rem" mb={6} opacity="0.3">Graduate Software Developer</Heading>
 			<Text maxW="xl" mb={8}>
 				I am a graduate <Text as="span" color="brand.red">developer </Text>
 				specializing in creating bespoke web experiences.
