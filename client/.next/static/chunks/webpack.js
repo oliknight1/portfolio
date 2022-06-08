b108c2dd95a719f32c402a306be9a35f9971556c
Oli Knight <olidknight@gmail.com>
2022-06-08 17:42:10 +0100

    Remove Strapi

--- client/.next/static/chunks/webpack.js
+++ client/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "934480cf98d78597"; }
+/******/ 		__webpack_require__.h = function() { return "b4524ab95746a777"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
