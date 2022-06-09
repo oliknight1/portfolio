a71834c0e983acb3ec2ce2bfd5827a9ed97c6d22
Oli Knight <olidknight@gmail.com>
2022-06-09 16:05:57 +0100

    Delete client directory

--- client/components/Footer.tsx
+++
@@ -1,7 +0,0 @@
-import { Center } from '@chakra-ui/react';
-import React, { FC } from 'react';
-
-const Footer : FC = () => (
-	<Center opacity={0.6} fontFamily="monospace" mb={16}>Designed and built by Oli Knight, 2022</Center>
-);
-export default Footer;
--- client/styles/general.ts
+++
@@ -1,8 +0,0 @@
-export const vertical_line = {
-	content: '""',
-	display: 'block',
-	width: '1px',
-	height: '100px',
-	margin: '0px auto',
-	background: 'white',
-};
